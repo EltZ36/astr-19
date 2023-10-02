5fd011af698ee1cc9b9306fc3390027ecdaf3404
Elton Z <poptako9@gmail.com>
2023-10-02 10:18:38 -0700

    Adding in session one

---
+++ session_one/main.py
@@ -0,0 +1,12 @@
+def info():
+    print("Full name: Elton Zeng")
+    print("pronouns: He/Him")
+    print("My favorite movie is Kung Fu Hustle (2004)")
+    print("My favorite food is salt & pepper shrimp")
+    return None
+
+def main():
+    info()
+
+if (__name__ == '__main__'):
+    main()
